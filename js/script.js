8071363e1dc48c14820e7d8227c8d23684baf9ee
sayma nowshin <124274759+saymanowshin9@users.noreply.github.com>
2023-07-04 19:52:14 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+ 
+$("#owl-demo").owlCarousel({
+ 
+navigation : true, 
+ 
+slideSpeed : 300,
+paginationSpeed : 400,
+ 
+items : 3, 
+itemsDesktop : false,
+itemsDesktopSmall : false,
+itemsTablet: false,
+itemsMobile : false,
+itemsMobile : [575,2]
+ 
+});
+ 
+});
